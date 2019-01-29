9a055e5852c81c0683b8b5a1c88623ec757ec4ae
Enrique Garcia <enriquegarga@gmail.com>
2019-01-29 00:23:30 -0600

    Imported Bootstrap Vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,6 +10,13 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 /**
+ * Importamos el paquete Bootstrap Vue
+ */
+
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue);
+
+/**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
